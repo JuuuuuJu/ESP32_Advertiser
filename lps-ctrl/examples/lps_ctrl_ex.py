90d23d264675dd35af904cee7eaf26ec8b7509c1
JuuuuuJu <y5c0901720@gmail.com>
2026-02-13 12:21:08 +0800

    update example

--- lps-ctrl/examples/lps_ctrl_ex.py
+++ lps-ctrl/examples/lps_ctrl_ex.py
@@ -6,16 +6,15 @@ def main():
     try:
         with ESP32BTSender(port=PORT) as sender:
 
-            # Cmd ID 0: Schedules PLAY to execute in 2.0s.
-            response = sender.send_burst(cmd_input='PLAY', delay_sec=2.0, prep_led_sec=0, target_ids=[], data=[0,0,0])
+            # Cmd ID 0: Schedules PLAY to execute in 10.0s, with prepare delay light playing for 5 seconds after command received by receiver.
+            response = sender.send_burst(cmd_input='PLAY', delay_sec=10.0, prep_led_sec=5, target_ids=[], data=[0,0,0])
             print(f"{json.dumps(response, indent=4, ensure_ascii=False)}")
             
-            # Cmd ID 1: Schedules PAUSE to execute in 5.0s.
-            response = sender.send_burst(cmd_input='PAUSE', delay_sec=5.0, prep_led_sec=0, target_ids=[], data=[0,0,0])
+            # Cmd ID 1: Schedules PAUSE to execute in 12.0s.
+            response = sender.send_burst(cmd_input='PAUSE', delay_sec=12, prep_led_sec=0, target_ids=[], data=[0,0,0])
             print(f"{json.dumps(response, indent=4, ensure_ascii=False)}")
 
             # Cmd ID 2: Cancels Cmd ID 1 (PAUSE) in 1.0s.
-            # Since 1.0s < 5.0s, the PAUSE action will be intercepted and stopped.
             response = sender.send_burst(cmd_input='CANCEL', delay_sec=1.0, prep_led_sec=0, target_ids=[], data=[1,0,0])
             print(f"{json.dumps(response, indent=4, ensure_ascii=False)}")
             
@@ -25,13 +24,13 @@ def main():
             response = sender.trigger_check()
             print(f"{json.dumps(response, indent=4, ensure_ascii=False)}")
             
-            # Cmd ID 4: Schedules STOP at 6.0s.
+            # Cmd ID 4: Schedules STOP at 14.0s.
             # This command is queued successfully even if the Sender is currently scanning.
-            response = sender.send_burst(cmd_input='STOP', delay_sec=6.0, prep_led_sec=0, target_ids=[], data=[0,0,0])
+            response = sender.send_burst(cmd_input='STOP', delay_sec=14.0, prep_led_sec=0, target_ids=[], data=[0,0,0])
             print(f"{json.dumps(response, indent=4, ensure_ascii=False)}")
 
-            # Cmd ID 5: Schedules TEST at 7.0s.
-            response = sender.send_burst(cmd_input='TEST', delay_sec=7.0, prep_led_sec=0, target_ids=[], data=[0,0,0])
+            # Cmd ID 5: Schedules TEST at 15.0s.
+            response = sender.send_burst(cmd_input='TEST', delay_sec=15.0, prep_led_sec=0, target_ids=[], data=[0,0,0])
             print(f"{json.dumps(response, indent=4, ensure_ascii=False)}")
 
             # Waits for the 2s scan window to finish, then fetches the aggregated report.
